219c8792a7684cab0ba6b0c412e253b77871d009
YowHobin <lruarellano@rtu.edu.ph>
2023-08-06 17:48:55 +0800

    finish legit

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -3,9 +3,10 @@ import { redirect } from "next/navigation";
 
 import UserCard from "@/components/cards/UserCard";
 import Pagination from "@/components/shared/Pagination";
-import Searchbar from "@/components/shared/Searchbar";
+
 
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import SearchbarUser from "@/components/shared/SearchbarUser";
 
 async function Page({
   searchParams,
@@ -29,7 +30,7 @@ async function Page({
     <section>
       <h1 className="head-text mb-10">Search</h1>
 
-      <Searchbar routeType="search" />
+      <SearchbarUser routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
