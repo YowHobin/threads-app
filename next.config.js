cbcc0ddba04737ff6c7234a8c2f7f6de7e591a61
YowHobin <lruarellano@rtu.edu.ph>
2023-08-06 17:26:07 +0800

    finish i guess

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const nextConfig = {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
